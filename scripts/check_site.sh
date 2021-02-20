3d8139ff399fa83cabade5b8befb080ee9236938
Brock Fanning <brockfanning@gmail.com>
2021-02-20 10:36:01 -0500

    Windows issues in html proofing

--- scripts/check_site.sh
+++ scripts/check_site.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 # The following script assumes the site is already built at _site.
-
 # Figure out the Jekyll baseurl. If this doesn't work for some reason, you can
 # just hardcode it, eg: JEKYLL_BASEURL="/my-baseurl"
 JEKYLL_BASEURL=$(
   grep "^baseurl: " config_site.yml | # Look for 'baseurl' in the Jekyll config.
   head -n1                      | # Only use the first one.
   awk '{ print $2}'             | # Use the value part, after the colon.
+  sed $'s/\r//'                 | # Strip Windows line endings.
   sed -e 's/^"//' -e 's/"$//'     # Strip any quotes.
 )
 # We have to create a temporary folder to test in, because html-proofer does not

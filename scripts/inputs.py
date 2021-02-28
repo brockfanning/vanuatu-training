0830170ff61be8c347f5c67ff61fce1d2ca3f405
Brock Fanning <brockfanning@gmail.com>
2021-02-28 17:16:26 -0500

    Example of CSV disaggregation

--- scripts/inputs.py
+++ scripts/inputs.py
@@ -1,10 +1,13 @@
 from InputNsdpDatabase import InputNsdpDatabase
 from sdg.inputs.InputYamlMeta import InputYamlMeta
+from sdg.inputs.InputCsvData import InputCsvData
 
 def get_inputs():
+    csv_input = InputCsvData(path_pattern='data/*.csv')
     nsdp_input = InputNsdpDatabase()
     indicator_config_input = InputYamlMeta(path_pattern='indicators/*.yml', git=False)
     return [
         nsdp_input,
+        csv_input,
         indicator_config_input,
     ]
